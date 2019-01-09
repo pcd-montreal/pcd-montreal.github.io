fa37a4914b00f6b0aefe46ddbc5681b9210a076b
yèct <yectocsalev@gmail.com>
2019-01-08 19:58:41 -0500

    ( .

--- sketch8.js
+++ sketch8.js
@@ -52,121 +52,121 @@ function drawJagged() {
 
 
 function drawCylinderAzul() {
-    translate(120, 0);
-    var rX = 0.000000001;
-    var ruidoLoco = noise(millis() * rX) * 40;
-    var sides = ruidoLoco;
+	translate(120, 0);
+	var rX = 0.000000001;
+	var ruidoLoco = noise(millis() * rX) * 40;
+	var sides = ruidoLoco;
 
-    ambientLight(255, 0, 0);
-    ambientMaterial(20, 0, 50, 60);
+	ambientLight(255, 0, 0);
+	ambientMaterial(20, 0, 50, 80);
 
-    //ambientMaterial(0, 100, 255, 180);
-    var angleIncrement = TWO_PI / sides;
-    beginShape(TRIANGLES);
-    for (var i = 0; i < sides; ++i) {
+	//ambientMaterial(0, 100, 255, 180);
+	var angleIncrement = TWO_PI / sides;
+	beginShape(TRIANGLES);
+	for (var i = 0; i < sides; ++i) {
 
-        var angle = map(i, -7, 0, 90, TWO_PI);
-        var rY = 0.0000001;
-        var rrX = 0.0000002;
-        var bR = noise(millis() * rY) * -1200;
-        var tR = noise(millis() * rrX) * 1200;
-        var bottomRadius = bR;
-        var topRadius = tR;
+		var angle = map(i, -7, 0, 90, TWO_PI);
+		var rY = 0.0000001;
+		var rrX = 0.0000002;
+		var bR = noise(millis() * rY) * -1200;
+		var tR = noise(millis() * rrX) * 1200;
+		var bottomRadius = bR;
+		var topRadius = tR;
 
-        // - - - - - - - - - - - - - - - - - - - -
-        var ang = map(i, 0, bR / 0.35, 800, PI / tR);
-        var rT = 0.000003;
-        var ruidoTall = noise(millis() * rT) * 1200;
-        var tall = ruidoTall * cos(ang);
+		// - - - - - - - - - - - - - - - - - - - -
+		var ang = map(i, 0, bR / 0.35, 800, PI / tR);
+		var rT = 0.000003;
+		var ruidoTall = noise(millis() * rT) * 1200;
+		var tall = ruidoTall * cos(ang);
 
-        vertex(topRadius * cos(mouseX), 0, topRadius * sin(angle));
-        vertex(bottomRadius * cos(angle), tall, bottomRadius * sin(angle));
-        angle += angleIncrement;
+		vertex(topRadius * cos(mouseX), 0, topRadius * sin(angle));
+		vertex(bottomRadius * cos(angle), tall, bottomRadius * sin(angle));
+		angle += angleIncrement;
 
-    }
-    endShape();
-    n += 5;
-    g += 0.015;
-    t += 5;
+	}
+	endShape();
+	n += 5;
+	g += 0.015;
+	t += 5;
 }
 
 
 function drawCylinderVerde() {
-    translate(130, 0);
-    var rX = 0.0000001;
-    var ruidoLoco = noise(millis() * rX) * 60;
-    var sides = ruidoLoco;
+	translate(130, 0);
+	var rX = 0.0000001;
+	var ruidoLoco = noise(millis() * rX) * 60;
+	var sides = ruidoLoco;
 
-    ambientLight(255, 255, 255);
-    ambientMaterial(255, 255, 255, 80);
+	ambientLight(255, 255, 255);
+	ambientMaterial(255, 255, 255, 140);
 
-    //ambientMaterial(0, 100, 255, 180);
-    var angleIncrement = TWO_PI / sides;
-    beginShape(TRIANGLES);
-    for (var i = 0; i < sides; ++i) {
+	//ambientMaterial(0, 100, 255, 180);
+	var angleIncrement = TWO_PI / sides;
+	beginShape(TRIANGLES);
+	for (var i = 0; i < sides; ++i) {
 
-        var angle = map(i, -7, 0, 90, TWO_PI);
-        var rY = 0.000000215
-        var rrX = 0.000001;
-        var bR = noise(millis() * rY) * -2800;
-        var tR = noise(millis() * rrX) * 1900;
-        var bottomRadius = bR;
-        var topRadius = tR;
+		var angle = map(i, -7, 0, 90, TWO_PI);
+		var rY = 0.000000215
+		var rrX = 0.000001;
+		var bR = noise(millis() * rY) * -2800;
+		var tR = noise(millis() * rrX) * 1900;
+		var bottomRadius = bR;
+		var topRadius = tR;
 
-        // - - - - - - - - - - - - - - - - - - - -
-        var ang = map(i, 0, bR / 0.35, 800, PI / tR);
-        var rT = 0.00000003;
-        var ruidoTall = noise(millis() * rT) * mouseY;
-        var tall = ruidoTall * cos(ang);
+		// - - - - - - - - - - - - - - - - - - - -
+		var ang = map(i, 0, bR / 0.35, 800, PI / tR);
+		var rT = 0.00000003;
+		var ruidoTall = noise(millis() * rT) * mouseY;
+		var tall = ruidoTall * cos(ang);
 
-        vertex(topRadius * cos(angle), 0, topRadius * sin(angle));
-        vertex(bottomRadius * cos(mouseY), tall, bottomRadius * sin(angle));
-        angle += angleIncrement;
+		vertex(topRadius * cos(angle), 0, topRadius * sin(angle));
+		vertex(bottomRadius * cos(mouseY), tall, bottomRadius * sin(angle));
+		angle += angleIncrement;
 
-    }
-    endShape();
-    n += 5;
-    g += 0.015;
-    t += 5;
+	}
+	endShape();
+	n += 5;
+	g += 0.015;
+	t += 5;
 }
 
 
 function drawCylinderTrian() {
-    var rX = 0.0000001;
-    var ruidoLoco = noise(millis() * rX) * 53;
-    var sides = ruidoLoco;
+	var rX = 0.0000001;
+	var ruidoLoco = noise(millis() * rX) * 53;
+	var sides = ruidoLoco;
 
-    ambientLight(255, 255, 255);
-    ambientMaterial(255, 150, 0, 200);
+	ambientLight(255, 255, 255);
+	ambientMaterial(255, 150, 0, 180);
 
-    //ambientMaterial(0, 100, 255, 180);
-    var angleIncrement = TWO_PI / sides;
-    beginShape(TRIANGLES);
-    for (var i = 0; i < sides; ++i) {
+	//ambientMaterial(0, 100, 255, 180);
+	var angleIncrement = TWO_PI / sides;
+	beginShape(TRIANGLES);
+	for (var i = 0; i < sides; ++i) {
 
-        var angle = map(i, -7, 0, 90, TWO_PI);
-        var rY = 0.0000021;
-        var rrX = 0.0000001;
-        var bR = noise(millis() * rY) * 10;
-        var tR = noise(millis() * rrX) * 4000;
-        var bottomRadius = bR;
-        var topRadius = tR;
+		var angle = map(i, -7, 0, 90, TWO_PI);
+		var rY = 0.0000021;
+		var rrX = 0.0000001;
+		var bR = noise(millis() * rY) * 10;
+		var tR = noise(millis() * rrX) * 4000;
+		var bottomRadius = bR;
+		var topRadius = tR;
 
-        // - - - - - - - - - - - - - - - - - - - -
-        var ang = map(i, 0, bR / 0.35, 800, PI / tR);
-        var rT = 0.0000003;
-        var ruidoTall = noise(millis() * rT) * mouseX;
-        var tall = ruidoTall * cos(ang);
+		// - - - - - - - - - - - - - - - - - - - -
+		var ang = map(i, 0, bR / 0.35, 800, PI / tR);
+		var rT = 0.0000003;
+		var ruidoTall = noise(millis() * rT) * mouseX;
+		var tall = ruidoTall * cos(ang);
 
-        vertex(topRadius * cos(mouseY), 0, topRadius * sin(angle));
-        vertex(bottomRadius * cos(angle), tall, bottomRadius * sin(angle));
-        angle += angleIncrement;
+		vertex(topRadius * cos(mouseY), 0, topRadius * sin(angle));
+		vertex(bottomRadius * cos(angle), tall, bottomRadius * sin(angle));
+		angle += angleIncrement;
 
-    }
-    endShape();
-    n += 5;
-    g += 0.015;
-    t += 5;
+	}
+	endShape();
+	n += 5;
+	g += 0.015;
+	t += 5;
 }
 
 
